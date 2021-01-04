2c89ce3b245804d74f2f0e2757cb228b15847a54
Hüseyin Yıldırım <huseyinyildirim@hotmail.com>
2021-01-05 02:40:06 +0300

    modeller eklendi

---
+++ models/adminModel.ts
@@ -0,0 +1,10 @@
+interface Admin {
+    id?: number;
+    name: string;
+    surname: string;
+    mail: string;
+    phone: string;
+    password: string;
+  }
+  
+  export default Admin;
---
+++ models/customerModel.ts
@@ -0,0 +1,10 @@
+interface Customer {
+    id?: number;
+    name: string;
+    surname: string;
+    mail: string;
+    phone: string;
+    password: string;
+  }
+  
+  export default Customer;

07321075f205a62acca8fe779da2676b6ece0306
Hüseyin Yıldırım <huseyinyildirim@hotmail.com>
2021-01-06 02:29:06 +0300

    static klasörler için tanımlama yapıldı

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, send } from "https://deno.land/x/oak/mod.ts";
 import * as flags from 'https://deno.land/std/flags/mod.ts';
 import {viewEngine, engineFactory, adapterFactory } from "https://deno.land/x/view_engine/mod.ts";
 import { green, yellow } from "https://deno.land/std@0.53.0/fmt/colors.ts";
@@ -9,7 +9,7 @@ import errorMiddleware from "./middlewares/error.ts";
 
 const {args, exit} = Deno;
 
-const DEFAULT_PORT = 5000;
+const DEFAULT_PORT = 5005;
 const argPort = flags.parse(args).port;
 const port = argPort ? Number(argPort) : DEFAULT_PORT;
 
@@ -25,8 +25,21 @@ const app = new Application();
 
 app.use(viewEngine(oakAdapter, ejsEngine, {
     viewRoot: "./views",
-    viewExt: ".ejs",
+    viewExt: ".ejs"
 }));
+
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.headers.get("X-Response-Time");
+    console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+app.use(async (ctx, next)=> {
+    const start = Date.now();
+    await next();
+    const ms = Date.now();
+    ctx.response.headers.set("X-Response-Time", `${ms - start}ms`);
+});
 
 app.use(errorMiddleware)
 app.use(router.routes());
@@ -36,14 +49,21 @@ app.addEventListener("listen", ({ secure, hostname, port }) => {
     const protocol = secure ? "https://" : "http://";
     const url = `${protocol}${hostname ?? "localhost"}:${port}`;
     console.log(`${green("Uygulama başladı.")} ${yellow(url)} portu dinleniyor!`);
-  });
+});
+
+app.use(async (ctx, next) => {
+    await send(ctx, ctx.request.url.pathname,{
+        root: `${Deno.cwd()}/static`
+    });
+    next();
+});
 
 await app.listen({ port: port });
 
 await client.close();
 
 // Deno, biz istemediğimiz sürece dosya okuma ve internete erişme izni vermez.
-// deno run --allow-env --allow-net --allow-read app.ts
+// deno run --allow-env --allow-net --allow-read --allow-write --allow-plugin --unstable app.ts
 
 // Live Reload
-// denon run --allow-env --allow-net --allow-read app.ts
+// denon run --allow-env --allow-net --allow-read --allow-write --allow-plugin --unstable app.ts

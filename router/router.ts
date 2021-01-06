8c4d634152528c8b46e31ade45695ee5fe0f4767
Hüseyin Yıldırım <huseyinyildirim@hotmail.com>
2021-01-06 17:02:13 +0300

    admin, api ve assets dosyalar için route tanımlandı

--- router/router.ts
+++ router/router.ts
@@ -5,30 +5,56 @@ import deviceTraceController from "../controllers/deviceTraceController.ts";
 
 const router = new Router();
 
+//#region Assets
+router.get("/assets/web/:folder/:file", async (context) => {
+    const folder = context.params.folder;
+    const file = context.params.file;
+    context.response.body = await Deno.readFile(`${Deno.cwd()}/assets/web/${folder}/${file}`);
+});
+router.get("/assets/admin/:folder/:file", async (context) => {
+    const folder = context.params.folder;
+    const file = context.params.file;
+    context.response.body = await Deno.readFile(`${Deno.cwd()}/assets/admin/${folder}/${file}`);
+});
+//#endregion
+
+//#region Web
 router.get('/', (ctx : any) => {
-    ctx.render("index", null);
+    ctx.render("web/pages/home/index", null);
 });
 
 router.get('/hakkimizda', (ctx : any) => {
-    ctx.render("about-us", null);
+    ctx.render("web/pages/content/about-us", null);
 });
 
 router.get('/iletisim', (ctx : any) => {
-    ctx.render("contact", null);
+    ctx.render("web/pages/content/contact", null);
 });
+////#endregion
 
-//router.get("/device/:id", deviceController.show);
-router.get("/device/:serialNo", deviceController.showBySerialNo);
-
-router.get("/device/:deviceId/traces", deviceTraceController.showByDeviceId);
-router.post("/device/:deviceId/traces", deviceTraceController.store);
-
+//#region 
 router.get('/admin', (ctx : any) => {
-    ctx.render("admin/index", null);
+    ctx.render("admin/pages/home/index", null);
 });
 
 router.get('/admin/login', (ctx : any) => {
-    ctx.render("admin/auth/login", null);
+    ctx.render("admin/pages/auth/login", null);
 });
+
+router.get('/admin/customer', (ctx : any) => {
+    ctx.render("admin/pages/customer/index", null);
+});
+
+router.get('/admin/device', (ctx : any) => {
+    ctx.render("admin/pages/device/index", null);
+});
+//#endregion
+
+//#region API
+//router.get("/device/:id", deviceController.show);
+router.get("/api/v1/device/:serialNo", deviceController.showBySerialNo);
+router.get("/api/v1/device/:deviceId/traces", deviceTraceController.showByDeviceId);
+router.post("/api/v1/device/:deviceId/traces", deviceTraceController.store);
+////#endregion
 
 export { router };

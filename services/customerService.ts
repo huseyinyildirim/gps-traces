f9f83c9a6f16267963240fdab114e100bf37b2a3
Hüseyin Yıldırım <huseyinyildirim@hotmail.com>
2021-01-07 09:00:13 +0300

    müşteriler için servis katmanı eklendş

---
+++ services/customerService.ts
@@ -0,0 +1,26 @@
+import CustomerRepository from "../repositories/customerRepository.ts";
+import CustomerModel from "../models/customerModel.ts";
+
+class CustomerService {
+  getAll = async () => {
+    return await CustomerRepository.all();
+  };
+
+  getById = async (id: number) => {
+    return await CustomerRepository.find(id);
+  };
+
+  create = async (device: CustomerModel) => {
+    return await CustomerRepository.create(device);
+  };
+
+  update = async (id: number, device: CustomerModel) => {
+    return await CustomerRepository.update(id, device);
+  };
+
+  delete = async (id: number) => {
+    return await CustomerRepository.delete(id);
+  };
+}
+
+export default new CustomerService();

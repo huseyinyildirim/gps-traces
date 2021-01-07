f0ea463ddb8e1ed6113b34bb06d9c07fde645f9e
Hüseyin Yıldırım <huseyinyildirim@hotmail.com>
2021-01-07 08:59:57 +0300

    auth ve customer controller eklendi

---
+++ controllers/admin/authController.ts
@@ -0,0 +1,92 @@
+import { RouterContext } from "https://deno.land/x/oak/mod.ts";
+import deviceService from "../../services/deviceService.ts";
+import deviceModel from "../../models/deviceModel.ts"
+import { RESPONSE_STATUS_TYPE } from "../../core/constants.ts";
+
+class AuthController {
+
+  async index(context: RouterContext) {
+    const data = await deviceService.getAll();
+    context.response.headers.set("Content-Type", "application/json");
+    context.response.body = { data: data };
+  }
+
+  async showBySerialNo(context: RouterContext) {
+    
+    if (context.params && context.params.serialNo) {
+
+      const { serialNo } = context.params;
+      const data = await deviceService.getBySerialNo(serialNo!);
+
+      context.response.headers.set("Content-Type", "application/json");
+
+      context.response.body = {
+        status: RESPONSE_STATUS_TYPE.success,
+        statusCode: 200,
+        systemTime: Date.now(),
+        data: data,
+        message: null,
+        error: {
+            message: null,
+            internalMessage: null,
+            help: null
+        }
+      };
+
+    } else {
+      context.response.body = {
+        status: RESPONSE_STATUS_TYPE.failure,
+        statusCode: 404,
+        systemTime: Date.now(),
+        data: null,
+        message: null,
+        error: {
+            message: null,
+            internalMessage: null,
+            help: null
+        }
+      };
+    }
+  }
+
+  async show(context: RouterContext) {
+    const { id } = context.params;
+    const device = await deviceService.getById(parseInt(id!));
+    context.response.headers.set("Content-Type", "application/json");
+    context.response.body = { data: device };
+  }
+
+  async store(context: RouterContext) {
+    const result = await context.request.body(
+      { contentTypes: { text: ["application/json"] } },
+    );
+    const device = result.value;
+
+    await deviceService.create(<deviceModel><unknown>device);
+
+    context.response.headers.set("Content-Type", "application/json");
+    context.response.body = { message: "success" };
+  }
+
+  async update(context: RouterContext) {
+    const result = await context.request.body(
+      { contentTypes: { text: ["application/json"] } },
+    );
+    const device = result.value;
+    const { id } = context.params;
+    await deviceService.update(parseInt(id!), <deviceModel><unknown>device);
+
+    context.response.headers.set("Content-Type", "application/json");
+    context.response.body = { message: "success" };
+  }
+
+  async delete(context: RouterContext) {
+    const { id } = context.params;
+    await deviceService.delete(parseInt(id!));
+
+    context.response.headers.set("Content-Type", "application/json");
+    context.response.body = { message: "success" };
+  }
+}
+
+export default new AuthController();
---
+++ controllers/admin/customerController.ts
@@ -0,0 +1,67 @@
+import { RouterContext } from "https://deno.land/x/oak/mod.ts";
+import customerService from "../../services/customerService.ts";
+import customerModel from "../../models/customerModel.ts"
+import { RESPONSE_STATUS_TYPE } from "../../core/constants.ts";
+
+class CustomerController {
+
+  async index(context: RouterContext) {
+    const data = await customerService.getAll();
+
+
+    return  {
+      status: RESPONSE_STATUS_TYPE.success,
+      statusCode: 200,
+      systemTime: Date.now(),
+      data: JSON.stringify(data),
+      message: null,
+      error: {
+        message: null,
+        internalMessage: null,
+        help: null
+      }
+    };
+
+  }
+
+  async show(context: RouterContext) {
+    const { id } = context.params;
+    const device = await customerService.getById(parseInt(id!));
+    context.response.headers.set("Content-Type", "application/json");
+    context.response.body = { data: device };
+  }
+
+  async store(context: RouterContext) {
+    const result = await context.request.body(
+      { contentTypes: { text: ["application/json"] } },
+    );
+    const device = result.value;
+
+    await customerService.create(<customerModel><unknown>device);
+
+    context.response.headers.set("Content-Type", "application/json");
+    context.response.body = { message: "success" };
+  }
+
+  async update(context: RouterContext) {
+    const result = await context.request.body(
+      { contentTypes: { text: ["application/json"] } },
+    );
+    const device = result.value;
+    const { id } = context.params;
+    await customerService.update(parseInt(id!), <customerModel><unknown>device);
+
+    context.response.headers.set("Content-Type", "application/json");
+    context.response.body = { message: "success" };
+  }
+
+  async delete(context: RouterContext) {
+    const { id } = context.params;
+    await customerService.delete(parseInt(id!));
+
+    context.response.headers.set("Content-Type", "application/json");
+    context.response.body = { message: "success" };
+  }
+}
+
+export default new CustomerController();
